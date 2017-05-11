07a5152d7d1d10caf6d1dc54d1acbe1a1d0d38cb
Neil Peterson <neilpeterson@outlook.com>
2017-05-10 21:00:31 -0700

    adding scirpt identifier

--- deployment/custom-script/deploy-app-secured-custom-script.sh
+++ deployment/custom-script/deploy-app-secured-custom-script.sh
@@ -1,24 +1,86 @@
 #!/bin/bash
 
 # VM values
-resourceGroup="myResourceGroup4"
-vmFront="vmfront4"
-vmBack="vmback4"
+resourceGroup="myResourceGroup6"
+vmFront="vmfront6"
+vmBack="vmback6"
 
 # Create resource group
 az group create --name $resourceGroup --location westus
 
-# Create a virtual network
-az network vnet create --resource-group $resourceGroup --name myVnet --subnet-name mySubnet
+# Create a virtual network and front-end subnet
+az network vnet create \
+  --resource-group $resourceGroup \
+  --name myVnet \
+  --address-prefix 10.0.0.0/16 \
+  --subnet-name mySubnetFrontEnd \
+  --subnet-prefix 10.0.1.0/24
+
+# Create back-end subnet
+  az network vnet subnet create \
+  --resource-group $resourceGroup \
+  --vnet-name myVnet \
+  --name mySubnetBackEnd \
+  --address-prefix 10.0.2.0/24
 
 # Pre-create back-end NSG
-az network nsg create --resource-group $resourceGroup --name myBackendNSG
-az network nsg rule create --resource-group $resourceGroup --nsg-name myBackendNSG --name mySQL --access Allow --protocol Tcp --direction Inbound --priority 200 --source-address-prefix 10.0.0.4 --source-port-range "*" --destination-address-prefix "*" --destination-port-range 3306
-az network vnet subnet create --address-prefix 10.0.1.0/24 --name myBackendSubnet --resource-group $resourceGroup --vnet-name myVnet --network-security-group myBackendNSG
+az network nsg create --resource-group $resourceGroup --name myNSGBackEnd
 
-# Create back-end VMs
-az vm create --resource-group $resourceGroup --name $vmBack --image UbuntuLTS  --generate-ssh-keys
-# az vm create --resource-group $resourceGroup --name $vmBack --image UbuntuLTS --public-ip-address "" --nsg "" --generate-ssh-keys
+az network vnet subnet update \
+  --resource-group $resourceGroup \
+  --vnet-name myVnet \
+  --name mySubnetBackEnd \
+  --network-security-group myNSGBackEnd
+
+ az network nsg rule create \
+  --resource-group $resourceGroup \
+  --nsg-name myNSGBackEnd \
+  --name SSH \
+  --access Allow \
+  --protocol Tcp \
+  --direction Inbound \
+  --priority 100 \
+  --source-address-prefix 10.0.1.0/24 \
+  --source-port-range "*" \
+  --destination-address-prefix "*" \
+  --destination-port-range "22" 
+
+az network nsg rule create \
+  --resource-group $resourceGroup \
+  --nsg-name myNSGBackEnd \
+  --name MySQL \
+  --access Allow \
+  --protocol Tcp \
+  --direction Inbound \
+  --priority 200 \
+  --source-address-prefix 10.0.1.0/24 \
+  --source-port-range "*" \
+  --destination-address-prefix "*" \
+  --destination-port-range "3306"
+
+az network nsg rule create \
+  --resource-group $resourceGroup \
+  --nsg-name myNSGBackEnd \
+  --name denyAll \
+  --access Deny \
+  --protocol Tcp \
+  --direction Inbound \
+  --priority 300 \
+  --source-address-prefix "*" \
+  --source-port-range "*" \
+  --destination-address-prefix "*" \
+  --destination-port-range "*"
+
+# Create back-end vm
+az vm create \
+  --resource-group $resourceGroup \
+  --name $vmBack \
+  --vnet-name myVnet \
+  --subnet mySubnetBackEnd \
+  --public-ip-address "" \
+  --nsg "" \
+  --image UbuntuLTS \
+  --generate-ssh-keys
 
 # configure back
 az vm extension set \
@@ -29,13 +91,22 @@ az vm extension set \
   --settings '{"fileUris": ["https://raw.githubusercontent.com/neilpeterson/flask-voting-app/master/deployment/custom-script/vote-app-back.sh"],"commandToExecute": "./vote-app-back.sh"}'
 
 # Create front-end
-az vm create --resource-group $resourceGroup --name $vmFront --image UbuntuLTS --vnet-name myVnet --generate-ssh-keys
+az vm create \
+  --resource-group $resourceGroup \
+  --name $vmFront \
+  --vnet-name myVnet \
+  --subnet mySubnetFrontEnd \
+  --nsg myNSGFrontEnd \
+  --public-ip-address myFrontEndIP \
+  --image UbuntuLTS \
+  --generate-ssh-keys
+
 az vm open-port --port 80 --resource-group $resourceGroup --name $vmFront
 
 # configure front
-# az vm extension set \
-# --resource-group $resourceGroup \
-# --vm-name $vmFront \
-# --name customScript \
-# --publisher Microsoft.Azure.Extensions \
-# --settings '{"fileUris": ["https://raw.githubusercontent.com/neilpeterson/flask-voting-app/master/deployment/custom-script/vote-app-front.sh"],"commandToExecute": "./vote-app-front.sh"}'
+az vm extension set \
+--resource-group $resourceGroup \
+--vm-name $vmFront \
+--name customScript \
+--publisher Microsoft.Azure.Extensions \
+--settings '{"fileUris": ["https://raw.githubusercontent.com/neilpeterson/flask-voting-app/master/deployment/custom-script/vote-app-front.sh"],"commandToExecute": "./vote-app-front.sh"}'
--- deployment/custom-script/vote-app-back.sh
+++ deployment/custom-script/vote-app-back.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 password=Password12
 
 sudo apt-get update
