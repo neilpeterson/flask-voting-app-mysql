c7e45f5975e52f7b71c3dda3a9c5cf618cefb69e
Neil Peterson <neilpeterson@outlook.com>
2017-05-12 14:38:16 -0700

    updates

--- deployment/vote-app-back.sh
+++ deployment/vote-app-back.sh
@@ -4,17 +4,17 @@
 user=$1
 password=$2
 
-Echo "----------: Installing MySQL : ----------"
+echo "----------: Installing MySQL : ----------"
 sudo apt-get update
 echo "mysql-server mysql-server/root_password password $password" | sudo debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password $password" | sudo debconf-set-selections
 sudo apt-get -y install mysql-server
 
-Echo "----------: Creating azurevote database : ----------"
+echo "----------: Creating azurevote database : ----------"
 mysqladmin -u root -p$password create azurevote
 sudo sed -i 's/bind-address/#bind-address/g' /etc/mysql/mysql.conf.d/mysqld.cnf
 
-Echo "----------: Creating user and application tables : ----------"
+echo "----------: Creating user and application tables : ----------"
 mysql -u root -p$password -Bse "CREATE USER $user@'localhost' IDENTIFIED BY '$password';"
 mysql -u root -p$password -Bse "GRANT ALL PRIVILEGES ON *.* TO $user@'localhost' WITH GRANT OPTION;"
 mysql -u root -p$password -Bse "CREATE USER $user@'%' IDENTIFIED BY '$password';"
@@ -22,7 +22,7 @@ mysql -u root -p$password -Bse "GRANT ALL PRIVILEGES ON *.* TO $user@'%' WITH GR
 mysql -u root -p$password -Bse "FLUSH PRIVILEGES;"
 mysql -u root -p$password -Bse 'CREATE TABLE `azurevote`.`azurevote` (`voteid` INT NOT NULL AUTO_INCREMENT,`votevalue` VARCHAR(45) NULL,PRIMARY KEY (`voteid`));'
 
-Echo "----------: Restart MySQL : ----------"
+echo "----------: Restart MySQL : ----------"
 sudo service mysql restart
 
-Echo "----------: Script complete : ----------"
+echo "----------: Script complete : ----------"
