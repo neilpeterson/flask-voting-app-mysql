d3db3399843a5ed74a8766c71e1bdad66c66dad9
Neil Peterson <neilpeterson@outlook.com>
2017-05-13 15:28:59 -0700

    new deployment types

---
+++ deployment/azure-docker-swarm/readme.md
@@ -0,0 +1,45 @@
+# Demo Azure voting app
+
+Simple Flask app for demonstrating Azure IaaS architectural configurations. The voting app consists of a Flask app and a MySQL database.
+
+![](../readme-media/vote-app.png)
+
+## Deployment
+
+Sample deployment scripts and cloud-init files are found in the deployment folder.
+
+Run these commands to deploy to your Azure subscription.
+
+```
+curl https://raw.githubusercontent.com/neilpeterson/flask-voting-app/master/deployment/cloud-init-front.txt > cloud-init-front.txt
+curl https://raw.githubusercontent.com/neilpeterson/flask-voting-app/master/deployment/cloud-init-back.txt > cloud-init-back.txt
+curl https://raw.githubusercontent.com/neilpeterson/flask-voting-app/master/deployment/deploy-app-secured.sh > deploy-app-secured.sh
+bash ./deploy-app-secured.sh
+``` 
+
+When deploying, the password value can be stored in Azure Key vault for maximum security. This configuration is not demonstrated in the deployment scripts. 
+
+## Configuration
+
+The `config_file.cfg` can be found at the root of the repo. This file can be used to configure MySQL connection string and basic UI settings.
+
+| Configuration | Description |
+|----|----|
+| TITLE | Title to be displayed in app and on title bar. |
+| VOTE1VALUE | This value will display as the first voting option. |
+| VOTE2VALUE| This value will display as the second voting option. |
+| SHOWHOST | If set to true, the title will be replaced with the name of the system or container hosting the application.  |
+
+```
+# MySql Configuration
+MYSQL_DATABASE_USER = 'dbuser'
+MYSQL_DATABASE_PASSWORD = 'Password12'
+MYSQL_DATABASE_DB = 'azurevote'
+MYSQL_DATABASE_HOST = '10.0.0.5'
+
+# UI Configurations
+TITLE = 'Azure Voting App'
+VOTE1VALUE = 'Cats'
+VOTE2VALUE = 'Dogs'
+SHOWHOST = 'false'
+```
---
+++ deployment/azure-vm/deploy-app-secured-custom-script.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+
+# MySQL values
+user="dbuser"
+password="Password12"
+
+# VM values
+resourceGroup="myResourceGroup"
+vmFront="vmfront"
+vmBack="vmback"
+
+# Create resource group
+az group create --name $resourceGroup --location eastus
+
+# Create a virtual network and front-end subnet
+az network vnet create \
+  --resource-group $resourceGroup \
+  --name myVnet \
+  --address-prefix 10.0.0.0/16 \
+  --subnet-name mySubnetFrontEnd \
+  --subnet-prefix 10.0.1.0/24
+
+# Create back-end subnet
+az network vnet subnet create \
+  --resource-group $resourceGroup \
+  --vnet-name myVnet \
+  --name mySubnetBackEnd \
+  --address-prefix 10.0.2.0/24
+
+# Pre-create back-end NSG
+az network nsg create --resource-group $resourceGroup --name myNSGBackEnd
+
+az network vnet subnet update \
+  --resource-group $resourceGroup \
+  --vnet-name myVnet \
+  --name mySubnetBackEnd \
+  --network-security-group myNSGBackEnd
+
+az network nsg rule create \
+  --resource-group $resourceGroup \
+  --nsg-name myNSGBackEnd \
+  --name SSH \
+  --access Allow \
+  --protocol Tcp \
+  --direction Inbound \
+  --priority 100 \
+  --source-address-prefix 10.0.1.0/24 \
+  --source-port-range "*" \
+  --destination-address-prefix "*" \
+  --destination-port-range "22" 
+
+az network nsg rule create \
+  --resource-group $resourceGroup \
+  --nsg-name myNSGBackEnd \
+  --name MySQL \
+  --access Allow \
+  --protocol Tcp \
+  --direction Inbound \
+  --priority 200 \
+  --source-address-prefix 10.0.1.0/24 \
+  --source-port-range "*" \
+  --destination-address-prefix "*" \
+  --destination-port-range "3306"
+
+az network nsg rule create \
+  --resource-group $resourceGroup \
+  --nsg-name myNSGBackEnd \
+  --name denyAll \
+  --access Deny \
+  --protocol Tcp \
+  --direction Inbound \
+  --priority 300 \
+  --source-address-prefix "*" \
+  --source-port-range "*" \
+  --destination-address-prefix "*" \
+  --destination-port-range "*"
+
+# Create back-end vm
+az vm create \
+  --resource-group $resourceGroup \
+  --name $vmBack \
+  --vnet-name myVnet \
+  --subnet mySubnetBackEnd \
+  --public-ip-address "" \
+  --nsg "" \
+  --image UbuntuLTS \
+  --generate-ssh-keys
+
+# configure back
+az vm extension set \
+  --resource-group $resourceGroup \
+  --vm-name $vmBack \
+  --name customScript \
+  --publisher Microsoft.Azure.Extensions \
+  --settings '{"fileUris": ["https://raw.githubusercontent.com/neilpeterson/flask-voting-app/master/deployment/vote-app-back.sh"]}' \
+  --protected-settings '{"commandToExecute": "./vote-app-back.sh '$user' '$password'"}'
+
+# Create front-end
+az vm create \
+  --resource-group $resourceGroup \
+  --name $vmFront \
+  --vnet-name myVnet \
+  --subnet mySubnetFrontEnd \
+  --nsg myNSGFrontEnd \
+  --public-ip-address myFrontEndIP \
+  --image UbuntuLTS \
+  --generate-ssh-keys
+
+# Front-end NSG rule
+az network nsg rule create \
+  --resource-group $resourceGroup \
+  --nsg-name myNSGFrontEnd \
+  --name http \
+  --access Allow \
+  --protocol Tcp \
+  --direction Inbound \
+  --priority 100 \
+  --source-address-prefix "*" \
+  --source-port-range "*" \
+  --destination-address-prefix "*" \
+  --destination-port-range "80" 
+
+
+# Get internal IP address of MySQL VM
+ip=$(az vm list-ip-addresses --resource-group $resourceGroup --name $vmBack --query [0].virtualMachine.network.privateIpAddresses[0] -o tsv)
+
+# configure front
+az vm extension set \
+  --resource-group $resourceGroup \
+  --vm-name $vmFront \
+  --name customScript \
+  --publisher Microsoft.Azure.Extensions \
+  --settings '{"fileUris": ["https://raw.githubusercontent.com/neilpeterson/flask-voting-app/master/deployment/vote-app-front.sh"]}' \
+  --protected-settings '{"commandToExecute": "./vote-app-front.sh '$user' '$password' '$ip'"}'
