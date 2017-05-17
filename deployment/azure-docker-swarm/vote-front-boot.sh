530e9465f97cd685d93762634f78982108418167
Neil Peterson <neilpeterson@outlook.com>
2017-05-17 16:27:17 -0700

    test

--- deployment/azure-docker-swarm/vote-front-boot.sh
+++ deployment/azure-docker-swarm/vote-front-boot.sh
@@ -1,3 +1,8 @@
+# Configuration values
+user=$1
+password=$2
+ip=$3
+
 # Update Azure vote config file
 sed -i "s/<user>/$user/g" /opt/vote-app/config_file.cfg
 sed -i "s/<password>/$password/g" /opt/vote-app/config_file.cfg
