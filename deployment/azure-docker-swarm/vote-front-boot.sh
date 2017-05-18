495df2576c55b0b5673d8cf98aa21ed3c149d14a
Neil Peterson <neilpeterson@outlook.com>
2017-05-17 19:16:44 -0700

    updates

--- deployment/azure-docker-swarm/vote-front-boot.sh
+++ deployment/azure-docker-swarm/vote-front-boot.sh
@@ -8,5 +8,7 @@ sed -i "s/<user>/$user/g" /opt/vote-app/config_file.cfg
 sed -i "s/<password>/$password/g" /opt/vote-app/config_file.cfg
 sed -i "s/<ip>/$ip/g" /opt/vote-app/config_file.cfg
 
+# Start NGINX
+
 # Start app
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
